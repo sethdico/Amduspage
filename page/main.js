e76236a89488f517e7cc994c999bea43766b7eeb
Amadeus <138116141+sethdico@users.noreply.github.com>
2025-12-19 23:14:50 +0800

    Update main.js

--- page/main.js
+++ page/main.js
@@ -2,12 +2,30 @@ module.exports = async function (event) {
   const config = require("../config.json");
   const api = {};
   
-  // Load basic API
-  api.sendMessage = require("./src/sendMessage")(event);
-  api.markAsSeen = require("./src/markAsSeen")(event);
+  // List of scripts to load from src/
+  const scripts = [
+    "markAsSeen",
+    "sendMessage",
+    "sendTypingIndicator", // Added
+    "setMessageReaction",  // Added
+    "sendAttachment"       // Added
+  ];
+
+  for (const scriptName of scripts) {
+    try {
+      // Load the script and pass the event to it
+      const loadedScript = require(`./src/${scriptName}`);
+      if (typeof loadedScript === "function") {
+        api[scriptName] = loadedScript(event);
+      }
+    } catch (e) {
+      console.error(`Failed to load API script: ${scriptName}`, e);
+    }
+  }
   
   global.api = api;
   global.PREFIX = config.PREFIX;
   
+  // Run the handler
   require("./handler.js")(event);
 };
