675d360707d1e472c12846c5c4a67265307822a2
Amadeus <138116141+sethdico@users.noreply.github.com>
2025-12-26 01:31:10 +0800

    Update sendTypingIndicator.js

--- page/src/sendTypingIndicator.js
+++ page/src/sendTypingIndicator.js
@@ -2,17 +2,12 @@ const axios = require("axios");
 
 module.exports = function (event) {
   return function sendTypingIndicator(isTyping, userId) {
-    const senderAction = isTyping ? "typing_on" : "typing_off";
+    const action = isTyping ? "typing_on" : "typing_off";
     const recipientID = userId || event.sender.id;
 
-    return axios.post(
-      `https://graph.facebook.com/v20.0/me/messages?access_token=${global.PAGE_ACCESS_TOKEN}`,
-      {
-        recipient: { id: recipientID },
-        sender_action: senderAction,
-      }
-    ).catch((err) => {
-      console.error("Typing Indicator Error:", err.response ? err.response.data : err.message);
-    });
+    return axios.post(`https://graph.facebook.com/v21.0/me/messages?access_token=${global.PAGE_ACCESS_TOKEN}`, {
+      recipient: { id: recipientID },
+      sender_action: action,
+    }).catch(() => {});
   };
 };
