1008159194ffafc0847255c26cef8f66431f0302
Amadeus <138116141+sethdico@users.noreply.github.com>
2026-01-31 22:21:10 +0800

    Update getUserInfo.js

--- page/src/getUserInfo.js
+++ page/src/getUserInfo.js
@@ -1,5 +1,4 @@
 const axios = require("axios");
-
 module.exports = function (event) {
   return async function getUserInfo(id) {
     const uid = id || event.sender.id;
@@ -8,9 +7,7 @@ module.exports = function (event) {
         `https://graph.facebook.com/${uid}?fields=first_name,last_name,profile_pic&access_token=${global.PAGE_ACCESS_TOKEN}`,
         { timeout: 5000 }
       );
-      
       if (!res.data || res.data.error) return { name: "Messenger User", pic: null };
-
       return {
         name: `${res.data.first_name || ""} ${res.data.last_name || ""}`.trim() || "Messenger User",
         pic: res.data.profile_pic
