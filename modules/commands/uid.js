136c87ecbc0bf748efebc6f7bc11c92bcf17cc4d
Amadeus <138116141+sethdico@users.noreply.github.com>
2026-02-17 16:21:03 +0800

    Refactor uid command to simplify user info retrieval

--- modules/commands/uid.js
+++ modules/commands/uid.js
@@ -1,34 +1,22 @@
-const db = require("../core/database");
-
 module.exports.config = {
     name: "uid",
+    author: "sethdico",
     category: "Utility",
     adminOnly: false,
     usePrefix: false
 };
 
-module.exports.run = async ({ event, args, api, reply }) => {
+module.exports.run = async function ({ event, args, api, reply }) {
     const targetID = args[0] || event.sender.id;
 
     try {
         const info = await api.getUserInfo(targetID);
-        const name = info ? `${info.first_name} ${info.last_name}` : "unknown user";
-        const dbUser = await db.getUserData(targetID);
         
-        let msg = `user info\n\nname: ${name}\nid: ${targetID}\n`;
-        
-        if (dbUser) {
-            msg += `msgs: ${dbUser.count}\n`;
-            msg += `last active: ${new Date(dbUser.lastActive).toLocaleDateString()}`;
-        } else {
-            msg += `status: not in database`;
-        }
-        
-        reply(msg);
-        
-        if (info && info.profile_pic) {
-            api.sendAttachment("image", info.profile_pic, event.sender.id);
-        }
+        const msg = ` user info\n\n` +
+                    `name: ${info.name}\n` +
+                    `psid: ${targetID}`;
+
+        reply(msg.toLowerCase());
     } catch (e) {
         reply(`id: ${targetID}`);
     }
