d9931d05bd2131e55180aa0f95eacc09eac75bd5
Amadeus <138116141+sethdico@users.noreply.github.com>
2026-03-26 10:51:39 +0800

    Update validation.js

--- modules/middleware/validation.js
+++ modules/middleware/validation.js
@@ -1,8 +1,8 @@
 const { sanitize } = require('../utils/helpers');
+const crypto = require('crypto');
 
 function validateInput(req, res, next) {
     if (req.body) {
-        // sanitize all text fields recursively
         const clean = (obj) => {
             if (typeof obj === 'string') return sanitize(obj);
             if (Array.isArray(obj)) return obj.map(clean);
@@ -15,30 +15,33 @@ function validateInput(req, res, next) {
             }
             return obj;
         };
-        
         req.body = clean(req.body);
     }
-    
     next();
 }
 
 function verifyWebhookSignature(req, res, next) {
     const signature = req.headers['x-hub-signature-256'];
-    
-    if (!signature || !process.env.APP_SECRET) {
+    const appSecret = process.env.APP_SECRET;
+
+    if (!signature || !appSecret) {
         return next();
     }
-    
-    const crypto = require('crypto');
-    const hash = crypto
-        .createHmac('sha256', process.env.APP_SECRET)
-        .update(JSON.stringify(req.body))
+
+    if (!req.rawBody) {
+        return res.sendStatus(400);
+    }
+
+    const signatureHash = signature.split('=')[1];
+    const expectedHash = crypto
+        .createHmac('sha256', appSecret)
+        .update(req.rawBody)
         .digest('hex');
-    
-    if (`sha256=${hash}` !== signature) {
+
+    if (signatureHash !== expectedHash) {
         return res.sendStatus(403);
     }
-    
+
     next();
 }
 
